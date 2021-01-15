1792a9f5ed0cb13aded7798d8358c20c3e2ba546
ChloePark85 <hapark85@gmail.com>
2021-01-15 23:06:05 +0900

    added tab bar icons

--- App.js
+++ App.js
@@ -8,6 +8,26 @@ import TabUserScreen from './src/user_tab';
 import TabSearchScreen from './src/search_tab';
 
 const Tab = createBottomTabNavigator();
+const TabBarIcon = (focused, name) => {
+  let iconImagePath;
+
+  if (name === 'Home') {
+    iconImagePath = require('./assets/pics/home.png')
+  } else if (name === 'Search') {
+    iconImagePath = require('./assets/pics/search.png')
+  } else if (name === 'User') {
+    iconImagePath = require('./assets/pics/user.png')
+  }
+
+  return (
+    <Image 
+      style={{
+        width: focused ? 30: 20,
+        height: focused ? 30:20,
+      }}
+      source = {iconImagePath}/>
+  )
+}
 
 export default function App() {
   return (
@@ -21,7 +41,14 @@ export default function App() {
           style: {
             backgroundColor: '#c6cbef'
           }
-        }}>
+        }}
+        screenOptions={({route})=>({
+          tabBarLabel: route.name, 
+          tabBarIcon: ({focused}) =>(
+            TabBarIcon(focused, route.name)
+          )
+        })}
+        >
         <Tab.Screen name ="Home" component={TabHomeScreen} />
         <Tab.Screen name ="Search" component={TabSearchScreen} />
         <Tab.Screen name ="User" component={TabUserScreen} />
